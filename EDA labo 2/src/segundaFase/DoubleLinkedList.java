becf4cd96b6989a80f92398f49a1961f980342d0
nachetttt <nacholopezest@gmail.com>
2025-10-31 11:24:02 +0100

    Update DoubleLinkedList.java
    Métodos acabados, entre ellos, el clone(), el iterator...

--- EDA labo 2/src/segundaFase/DoubleLinkedList.java
+++ EDA labo 2/src/segundaFase/DoubleLinkedList.java
@@ -69,27 +69,33 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	public T remove(T elem) {
 	//Elimina un elemento concreto de la lista
 		// CALCULAR EL COSTE
+		boolean enc = false;
+		T dato = null;
 		if (!isEmpty()) {
 			Node<T> act = last.next;
 		    do {
 		        if (act.data.equals(elem)) {
-		            T dato = act.data;
-		            if (act == act.next) {
-		                last = null;
-		            } else {
-		                act.prev.next = act.next;
-		                act.next.prev = act.prev;
-		                if (act == last) {
-		                    last = act.prev;
-		                }
-		            }
-		            count--;
-		            return dato;
+		           enc = true;
+				}else{
+					act = act.next;
+				}
+			}while (act!=last.next && !enc);
+			if (enc){
+				if (act.next == act){
+					dato = act.data;
+					last = null;
+				}else{
+					dato = act.data;
+					act.prev.next = act.next;
+					act.next.prev = act.prev;
+					if (last == act){
+						last = last.prev;
+					}
 		        }
-		        act = act.next;
-		    } while (act != last.next);
+				count--;
+			}
 		}
-		return null;
+		return dato; 
 	}
 	
 	public void removeAll(T elem) {
@@ -109,30 +115,56 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 						}
 					}
 					count--;
+				}else{
+					act = act.next;
 				}
-				act = act.next;
-			} while (last != null && act != last.next);
+			} while (act.prev!=last);
 		}
 	}
 
 	public T first() {
 	//Da acceso al primer elemento de la lista
-		Node<T> first = last.next;
-			return first.data;
+		T first = null;
+		if (!isEmpty()){
+			first = last.next.data;
 		}
-
+		return first;
+	}
 	public T last() {
 	//Da acceso al �ltimo elemento de la lista
-		return last.data;
+		T ultimo = null;
+		if (!isEmpty()) { 
+			ultimo = last.data;
+		}
+		return ultimo;
 	}
 
 	public DoubleLinkedList<T> clone(){
 		// Devuelve una copia de la lista (no duplica el puntero)
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-		
-		//TODO hacer este método
-	} 
-
+		DoubleLinkedList<T> lista = new DoubleLinkedList<T>();
+		lista.last = null;
+		if (last!=null) {
+			Node<T> act = last.next;
+			do {
+				Node<T> nuevo = new Node<T>(act.data);
+				if (lista.last == null) {
+					lista.last = nuevo;
+					nuevo.next = nuevo;
+					nuevo.prev = nuevo;
+				}else {
+					nuevo.prev = lista.last;
+					nuevo.next = lista.last.next;
+					lista.last.next.prev = nuevo;
+					lista.last.next = nuevo;
+					lista.last = nuevo;
+				}
+				lista.count++;
+				act = act.next;
+			}while(act.prev!=last);
+		}
+		return lista;
+	}
 
 	public boolean contains(T elem) {
 	//Determina si la lista contiene un elemento concreto
@@ -146,23 +178,22 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	public T find(T elem) {
 	//Determina si la lista contiene un elemento concreto, y develve su referencia, null en caso de que no est�
 		Node<T> act = null;
+		T dato = null;
 		boolean enc = false;
 		if(!isEmpty()) {
-			act=last;
+			act = last.next;
 			do {
 				if(act.data.equals(elem)) {
 					enc=true;
+				}else{
+					act = act.next;
 				}
-				
-			}while(act!=last && !enc);				
-		}
-		if(enc) {
-			return act.data; //TODO por qué me da error?
-		}else {
-			return null;
+			}while(act.prev!=last && !enc);				
+			if(enc) {
+				dato = act.data; 
+			}
 		}
 	}
-
 	public boolean isEmpty(){ 
 	//Determina si la lista est� vac�a
 		boolean vacía = false;
@@ -179,36 +210,56 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	
 	/** Return an iterator to the stack that iterates through the items . */ 
 	public Iterator<T> iterator() {
-		
 		return new ListIterator(); 
 	} 
-
 	   // an iterator, doesn't implement remove() since it's optional 
-	   private class ListIterator implements Iterator<T> { 
-
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
-		   //TODO hacer este método
-
-	   } // private class
-		
-		
-         public void visualizarNodos() {
-			System.out.println(this.toString());
+	private class ListIterator implements Iterator<T> { 
+		Node<T> act;
+		int cont;
+		public ListIterator() {
+			cont = 0;
+			if (isEmpty()) {
+				act = null;
+			}else {
+				act = last.next;
+			}
 		}
 
 		@Override
-		public String toString() {
-			String result = new String();
-			Iterator<T> it = iterator();
-			while (it.hasNext()) {
-				T elem = it.next();
-				result = result + "[" + elem.toString() + "] \n";
-			}	
-			return "DoubleLinkedList " + result + "]";
+		public boolean hasNext() {
+			boolean tiene = false;
+			if (cont != count) {
+				tiene = true;
+			}
+			return tiene;
 		}
 
+		@Override
+		public T next() {
+			T dato = act.data;
+			act = act.next;
+			cont++;
+			return dato;
+		}
+	} // private class
+		
+    public void visualizarNodos() {
+		System.out.println(this.toString());
+	}
+
+	@Override
+	public String toString() {
+		String result = new String();
+		Iterator<T> it = iterator();
+		while (it.hasNext()) {
+			T elem = it.next();
+			result = result + "[" + elem.toString() + "] \n";
+		}	
+		return "DoubleLinkedList " + result + "]";
+	}
+
 }
+
 
 
 
