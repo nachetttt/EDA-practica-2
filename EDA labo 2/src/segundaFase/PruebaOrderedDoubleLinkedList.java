18cce2c571d3f55f8ee7f94c650f96c28ea0b554
nachetttt <nacholopezest@gmail.com>
2025-11-04 17:18:06 +0100

    Update PruebaOrderedDoubleLinkedList.java

--- EDA labo 2/src/segundaFase/PruebaOrderedDoubleLinkedList.java
+++ EDA labo 2/src/segundaFase/PruebaOrderedDoubleLinkedList.java
@@ -5,6 +5,28 @@ public class PruebaOrderedDoubleLinkedList {
 		public static void main(String[] args)  {
 			
 			OrderedDoubleLinkedList<Integer> l = new OrderedDoubleLinkedList<Integer>();
+			OrderedDoubleLinkedList<Integer> lo = new OrderedDoubleLinkedList<Integer>();
+			OrderedDoubleLinkedList<Integer> lo1 = new OrderedDoubleLinkedList<Integer>();
+			OrderedDoubleLinkedList<Integer> lo2 = new OrderedDoubleLinkedList<Integer>();
+			
+			lo.last=null;
+			lo.add(1);
+			lo.add(16);
+			lo.add(4);
+			lo.add(12);
+			lo.add(8);
+			lo.add(5);
+			
+			lo1.last=null;
+			lo1.add(1);
+			lo1.add(16);
+			lo1.add(43);
+			lo1.add(22);
+			lo1.add(5);
+			
+			
+			
+			
 			l.add(1);
 			l.add(3);
 			l.add(6);
@@ -26,7 +48,8 @@ public class PruebaOrderedDoubleLinkedList {
 			System.out.println("9? " + l.find(9));
 			System.out.println("0? " + l.find(0));
 			System.out.println("7? " + l.find(7));
-			
+			System.out.println("--------------------------------------");
+
 			
 			
 			OrderedDoubleLinkedList<Persona> l2 = new OrderedDoubleLinkedList<Persona>();
@@ -50,6 +73,19 @@ public class PruebaOrderedDoubleLinkedList {
 			System.out.println("5555? " + l2.find(new Persona("", "5555")));
 			System.out.println("7777? " + l2.find(new Persona("", "7777")));	
 			System.out.println("8888? " + l2.find(new Persona("", "8888")));	
+			System.out.println("--------------------------------------");
+
+			
+			//Comprueba que el metodo intersection devuelve la intersección
+			System.out.println("· Prueba intersection()");
+			System.out.print(" Lista ...............");
+			lo2.last=null;
+			lo2 = lo.intersection(lo1);
+			System.out.println("La lista debería devolver 1,16,5, y devuelve: ");
+			System.out.println("1?:"+lo2.find(1));
+			System.out.println("16?:"+lo2.find(16));
+			System.out.println("5?:"+lo2.find(5));
+			System.out.println("43?:"+lo2.find(43));
 			
 			
 	}
